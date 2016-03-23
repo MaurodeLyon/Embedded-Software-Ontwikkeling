58a71e5d74688b251dce3cb0b93aa036a6248e74
Mauro de Lyon <maurodelyon@gmail.com>
2016-03-23 12:22:47 +0100

    start of assesment requirments

--- Week 1/1/1.cpp
+++ Week 1/1/1.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
-void main() 
+void main()
 {
 	printf("hello, ");
 	printf("world");
 	printf("\n");
+
+	int a = 5;
+	int *pa;
+	pa = &a;
+	printf("the value of a    :\t%d\n", a);
+	printf("the address of a  :\t%d\n", &a);
+	printf("pointer to address:\t%d\n", pa);
+	printf("address of pointer:\t%d\n", &pa);
+	printf("dereference of pa :\t%d\n", *pa);
 }
